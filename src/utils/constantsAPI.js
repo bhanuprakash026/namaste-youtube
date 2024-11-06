e4ce1ed2e19ae2d5db73d0f9a87a218da5894c65
bhanuprakash026 <bhanutankasala26@gmail.com>
2024-11-07 00:23:42 +0530

    adding back to API file

---
+++ src/utils/constantsAPI.js
@@ -0,0 +1,30 @@
+export const GOOGLE_API_KEY = "AIzaSyDslI2fG-WTvJ2r8E4ZbUAJ3EgS1VQeG88" // "AIzaSyB_PiF37026pXVW_eDGvT4V5tplStX-P3A" // "AIzaSyBD2d7tb5kQCuXYyPXE3049oZ0aiqr4Hws" //"AIzaSyB_PiF37026pXVW_eDGvT4V5tplStX-P3A"
+export const YOUTUBE_VIDEO_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=100&regionCode=IN&type=video&key=" +GOOGLE_API_KEY
+export const YOUTUBE_SEARCH_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
+export const COMMENTS_API = "https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&videoId="
+export const LIVE_CHAT_COUNT = 25
+export const VIDEO_DETAILS = 'https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&id='
+export const SUGGESTIONS_VIDEOS_API = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&type=video&key='+ GOOGLE_API_KEY
+export const channelIdByName = 'https://www.googleapis.com/youtube/v3/search?part=snippet&type=channel&key='+ GOOGLE_API_KEY
+export const channelDetailsAPI = 'https://www.googleapis.com/youtube/v3/channels?part=snippet,contentDetails,brandingSettings,statistics&id='
+export const API = 'https://www.googleapis.com/youtube/v3/channels?part=snippet,contentDetails,brandingSettings,statistics&id=UCOJlrO9zmDYSlIKrwbUg1hQ&key=AIzaSyBD2d7tb5kQCuXYyPXE3049oZ0aiqr4Hws'
+export const searchAPI = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&type=video,playlists&key='+ GOOGLE_API_KEY
+export const LATEST_VIDEOS_IN_A_CHANNEL = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails&maxResults=12&playlistId='
+export const SHORTS_VIDEOS_IN_CHANNEL = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&type=video&videoDuration=short&key='+ GOOGLE_API_KEY
+export const POPULAR_VIDEOS_IN_CHANNEL = 'https://www.googleapis.com/youtube/v3/search?part=snippet&order=viewCount&type=video&maxResults=15&key=' + GOOGLE_API_KEY
+export const VIDEOS_IN_CHANNEL = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails&maxResults=20&key=' + GOOGLE_API_KEY + '&playlistId='
+export const SHORT_VIDEOS = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=shorts&type=video&videoDuration=short&key=' + GOOGLE_API_KEY
+export const SHORT_VIDEO_DETAILS = 'https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&key='+ GOOGLE_API_KEY 
+
+// const apurva19_API_KEY9 = "AIzaSyBD2d7tb5kQCuXYyPXE3049oZ0aiqr4Hws";
+// const apurva19_API_KEY10 = "AIzaSyArnnLGA0YQJKJXjiJsv5RhQMoPyXoKcUE";
+// const app_gaurav_API_KEY1 = "AIzaSyA3oBuUyn8nKJGB4hKFS0zO52xlv2TXMnU";
+// const family4_API_KEY = "AIzaSyBzIKrj9PVv0ULeKVX3n7-CuxKydNTE0xc";
+// const cimageAccount_API_KEY = "AIzaSyCXTR8i4IFoeuzN_hlSK1y-XslncdbV5Sw";
+// const tbp026 = "AIzaSyDslI2fG-WTvJ2r8E4ZbUAJ3EgS1VQeG88"
+
+
+
+
+
+
